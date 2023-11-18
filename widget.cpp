a6d2ed53ca3cdc38215f0ea2c355f8cf7d988607
QiangOrz <125181176+QiangOrz@users.noreply.github.com>
2023-11-18 10:36:21 +0800

    Update widget.cpp

--- widget.cpp
+++ widget.cpp
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 ﻿#include "widget.h"
 #include "ui_widget.h"
 
@@ -17,20 +17,4 @@ Widget::~Widget()
     delete ui;
 }
 
-=======
-#include "widget.h"
-#include "ui_widget.h"
 
-Widget::Widget(QWidget *parent)
-    : QWidget(parent)
-    , ui(new Ui::Widget)
-{
-    ui->setupUi(this);
-}
-
-Widget::~Widget()
-{
-    delete ui;
-}
-
->>>>>>> c12174bc4554224761f8ae5718372b30c18db966
